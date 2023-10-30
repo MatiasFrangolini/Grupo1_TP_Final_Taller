140f24f38eba4d463e8e56310801b55a4aa78276
Matias Frangolini <124532773+MatiasFrangolini@users.noreply.github.com>
2023-10-30 10:11:08 -0300

    calculaComision

--- testModeloDatos/TestEmpleador.java
+++ testModeloDatos/TestEmpleador.java
@@ -7,11 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import modeloDatos.Empleador;
+import modeloDatos.Ticket;
 
 public class TestEmpleador {
-
+	
+	private Ticket ticket;
+	
 	@Before
 	public void setUp() throws Exception {
+		ticket = new Ticket("HOMEOFFICE",1000,"JORNADA_MEDIA","JUNIOR","EXP_MEDIA", "PRIMARIOS");
 	}
 
 	@After
@@ -27,6 +31,21 @@ public class TestEmpleador {
 		assertEquals("El telefono no es el esperado", "2235825715", e.getTelefono());
 		assertEquals("El rubro no es el esperado", "COMERCIO_LOCAL", e.getRubro());
 		assertEquals("El tipo de persona no es el esperado", "FISICA", e.getTipoPersona());
+	}
+	
+	public void testCalculaComisionSalud() {
+		Empleador e = new Empleador("MatiF", "123456", "Matias", "2235825715", "SALUD", "FISICA");
+		assertTrue("El cálculo de la comisión es incorrecto", 600.0  == e.calculaComision(ticket));
+	}
+	
+	public void testCalculaComisionLocal() {
+		Empleador e = new Empleador("MatiF", "123456", "Matias", "2235825715", "COMERCIO_LOCAL", "FISICA");
+		assertTrue("El cálculo de la comisión es incorrecto", 700.0  == e.calculaComision(ticket));
+	}
+
+	public void testCalculaComisionInternacional() {
+		Empleador e = new Empleador("MatiF", "123456", "Matias", "2235825715", "COMERCIO_INTERNACIONAL", "FISICA");
+		assertTrue("El cálculo de la comisión es incorrecto", 800.0  == e.calculaComision(ticket));
 	}
 
 }
